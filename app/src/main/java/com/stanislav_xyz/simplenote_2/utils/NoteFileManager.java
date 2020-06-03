67dff0e5923c454cfea43b36b31dcdcc21d644ea
Stanislav <st2s3@mail.ru>
2020-06-03 21:09:18 +0300

    Permissions. Finished

--- app/src/main/java/com/stanislav_xyz/simplenote_2/utils/NoteFileManager.java
+++ app/src/main/java/com/stanislav_xyz/simplenote_2/utils/NoteFileManager.java
@@ -1,15 +1,7 @@
 package com.stanislav_xyz.simplenote_2.utils;
 
-
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
 import android.media.MediaScannerConnection;
-import android.os.Environment;
-import android.widget.Toast;
-
-import com.stanislav_xyz.simplenote_2.R;
-import com.stanislav_xyz.simplenote_2.activities.SettingsActivity;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,7 +10,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-import androidx.core.content.ContextCompat;
 
 public class NoteFileManager {
 
@@ -30,78 +21,58 @@ public class NoteFileManager {
     }
 
     public void exportTextToFile(File file, String body) {
-        if (isExternalStorageWritable() && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            try {
-                FileOutputStream fos = new FileOutputStream(file);
-                fos.write(body.getBytes());
-                fos.close();
-                scanFile(mContext, file, getMime(file));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else Utils.showToast(mContext, R.string.mes_permission_denied);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(body.getBytes());
+            fos.close();
+            scanFile(mContext, file, getMime(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void writeObjectsToFile(File file, Object... objects) {
-        if (isExternalStorageWritable() && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            try {
-                FileOutputStream fos = new FileOutputStream(file);
-                ObjectOutputStream oos = new ObjectOutputStream(fos);
-                // Сперва записывем число, равное количеству записываемых объектов
-                oos.writeInt(objects.length);
-                // Затем записываем сами объекты
-                for (Object object : objects)
-                    oos.writeObject(object);
-                fos.close();
-                oos.close();
-                Utils.showToast(mContext, "File Saved!");
-              scanFile(mContext, file, getMime(file));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else Utils.showToast(mContext, R.string.mes_permission_denied);
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            // Сперва записывем число, равное количеству записываемых объектов
+            oos.writeInt(objects.length);
+            // Затем записываем сами объекты
+            for (Object object : objects)
+                oos.writeObject(object);
+            fos.close();
+            oos.close();
+            Utils.showToast(mContext, "File Saved!");
+            scanFile(mContext, file, getMime(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public Object[] readObjectsFromFile(File file) {
-        if (isExternalStorageReadable() && checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            try {
-                FileInputStream fis = new FileInputStream(file);
-                ObjectInputStream ois = new ObjectInputStream(fis);
-                // Читаем из файла int, в котором записано число находящихся в файле объектов
-                int arrayLength = ois.readInt();
-                Object[] objects = new Object[arrayLength];
-                // Затем читаем сами объекты
-                for (int i = 0; i < arrayLength; i++)
-                    objects[i] = ois.readObject();
-                fis.close();
-                ois.close();
-                Utils.showToast(mContext, "File Loaded!");
-                return objects;
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-        } else Utils.showToast(mContext, R.string.mes_permission_denied);
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            // Читаем из файла int, в котором записано число находящихся в файле объектов
+            int arrayLength = ois.readInt();
+            Object[] objects = new Object[arrayLength];
+            // Затем читаем сами объекты
+            for (int i = 0; i < arrayLength; i++)
+                objects[i] = ois.readObject();
+            fis.close();
+            ois.close();
+            Utils.showToast(mContext, "File Loaded!");
+            return objects;
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     // Позволяет проводнику Windows находить созданный файл
     private void scanFile(Context context, File file, String mimeType) {
-        MediaScannerConnection.scanFile(context, new String[] {file.getAbsolutePath()},
-                new String[] {mimeType}, null);
-    }
-
-    private boolean isExternalStorageWritable() {
-        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
-    }
-
-    private boolean isExternalStorageReadable() {
-        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState()) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(Environment.getExternalStorageState());
-    }
-
-    private boolean checkPermission(String permission) {
-        int check = ContextCompat.checkSelfPermission(mContext, permission);
-        return (check == PackageManager.PERMISSION_GRANTED);
+        MediaScannerConnection.scanFile(context, new String[]{file.getAbsolutePath()},
+                new String[]{mimeType}, null);
     }
 
     // Возвращает mime из имени файла
